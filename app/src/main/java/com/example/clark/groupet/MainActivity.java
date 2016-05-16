e5a8894309c788409157958c1abde6ea63166b03
caok <caok1231@gmail.com>
2016-05-16 21:10:29 +0800

    add video ad

--- app/src/main/java/com/example/clark/groupet/MainActivity.java
+++ app/src/main/java/com/example/clark/groupet/MainActivity.java
@@ -4,18 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.KeyEvent;
-//import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebSettings;
-import com.mopub.mobileads.MoPubErrorCode;
 import com.mopub.mobileads.MoPubView;
+import android.view.View;
+import android.widget.Button;
+//import android.widget.ProgressBar;
 
+import com.jirbo.adcolony.AdColony;
+import com.jirbo.adcolony.AdColonyAd;
+import com.jirbo.adcolony.AdColonyAdAvailabilityListener;
+import com.jirbo.adcolony.AdColonyAdListener;
+import com.jirbo.adcolony.AdColonyVideoAd;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdColonyAdAvailabilityListener, AdColonyAdListener {
+    final private String APP_ID  = "app17ac68314d3d4ec79a";
+    final private String ZONE_ID = "vz4a146b857ab94aa699";
+    private AdColonyVideoAd ad;
+
     private WebView mWebView;
     private MoPubView moPubView;
 
+    private Button button;
+    //private ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +47,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        initBannerAd();
+        initVideoAd();
+    }
 
+    private void initBannerAd() {
         final String adUnitId = "497c211508fb480386cb5e2a500c0f01";
         moPubView = (MoPubView) findViewById(R.id.adview);
         moPubView.setAdUnitId(adUnitId);
         moPubView.setAutorefreshEnabled(true);
         moPubView.loadAd();
+    }
+
+    private void initVideoAd() {
+        AdColony.configure( this, "version:1.0,store:google", APP_ID, ZONE_ID );
+        AdColony.addAdAvailabilityListener( this );
+
+        //progress = (ProgressBar)findViewById( R.id.progress );
+
+        button = (Button)findViewById( R.id.button );
+        button.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                AdColonyVideoAd ad = new AdColonyVideoAd( ZONE_ID ).withListener( MainActivity.this );
+                ad.show();
+            }
+        } );
+
+        //If ads are already ready for zone (i.e. if onCreate is happening for a second time) we want
+        //to make sure our button is enabled.
+        if (AdColony.statusForZone( ZONE_ID ).equals( "active" ))
+        {
+            button.setEnabled( true );
+            //progress.setVisibility( View.INVISIBLE );;
+        }
     }
 
     @Override
@@ -60,4 +102,58 @@ public class MainActivity extends AppCompatActivity {
             moPubView = null;
         }
     }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        AdColony.pause();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        AdColony.resume( this );
+    }
+
+    @Override
+    public void onAdColonyAdAvailabilityChange( final boolean available, String zone_id )
+    {
+        runOnUiThread( new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                if (available)
+                {
+                    button.setEnabled( true );
+                    //progress.setVisibility( View.INVISIBLE );
+                }
+                else
+                {
+                    button.setEnabled( false );
+                    //progress.setVisibility( View.VISIBLE );
+                }
+            }
+        } );
+    }
+
+    @Override
+    public void onAdColonyAdAttemptFinished( AdColonyAd ad )
+    {
+        //Can use the ad object to determine information about the ad attempt:
+        //ad.shown();
+        //ad.notShown();
+        //ad.canceled();
+        //ad.noFill();
+        //ad.skipped();
+    }
+
+    @Override
+    public void onAdColonyAdStarted( AdColonyAd ad )
+    {
+        //Called when the ad has started playing
+    }
+
 }
